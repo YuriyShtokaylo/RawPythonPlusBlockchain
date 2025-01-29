9f8cea50ee7193609233dc952a46c72760b77740
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-01-29 16:44:53 +0100

    Code to be improved

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,19 @@
-blockchain = [1]
-
-def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
-    print(blockchain)    
+blockchain = []
 
 
-add_value(2)
-add_value(.9)
-add_value(10.89)
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])  
+
+
+tx_amount = input('Your transaction amount please: ')
+add_value(float(tx_amount))
+tx_amount = input('Your transaction amount please: ')
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+tx_amount = input('Your transaction amount please: ')
+add_value(tx_amount, get_last_blockchain_value())
+
+print(blockchain)
