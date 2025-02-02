cf5f53190f47e62f2563166b8145ca290906fac4
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-02 21:50:03 +0100

    Use reduce in application

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,5 @@
+import functools
+
 MINING_REWARD = 10
 SYSTEM_ACCOUNT = 'MINING'
 
@@ -124,15 +126,9 @@ def get_balance(participant):
     tx_sender = [[tx[AMOUNT] for tx in block[TRANSACTIONS] if tx[SENDER] == participant] for block in blockchain]
     open_tx_sender = [tx[AMOUNT] for tx in open_transactions if tx[SENDER] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender[1:]:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
     tx_recipient = [[tx[AMOUNT] for tx in block[TRANSACTIONS] if tx[RECIPIENT] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient[1:]:
-        if len(tx) > 0:
-            amount_received += tx[0]        
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)   
     return amount_received - amount_sent
                     
 
