0e4ede6ed8ec2beadc57611ae1d0c97cfee33a3e
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-02 21:09:26 +0100

    Reduce example

--- map_AND_lambda_AND_reduce.py
+++ map_AND_lambda_AND_reduce.py
@@ -1,4 +1,6 @@
-simple_list = [1, 2, 3, 4]
+import functools #Is used for reduce
+
+simple_list = [1, 2, 3, 4, 5]
 def mul2(el):
     return el * 2
 
@@ -10,3 +12,7 @@ print(r2)
 
 r_with_lambda = list(map(lambda el: el * 2, simple_list))
 print(r_with_lambda)
+
+s = functools.reduce(lambda pr, el: pr + el * 1, simple_list, 0)
+
+print(s)
