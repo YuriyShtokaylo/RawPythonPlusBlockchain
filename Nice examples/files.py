4e44ce86b1b39b2a48a10a20c0b7f7a64020a0ce
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-05 18:40:38 +0100

    Why we use file.close

---
+++ Nice examples/files.py
@@ -0,0 +1,5 @@
+file = open('demo.txt', mode='w')
+file.write('Hello World from Python')
+file.close() #If remove this file wouldn't be updated before code is fully executed
+
+user_input = input('Please enter input:')
