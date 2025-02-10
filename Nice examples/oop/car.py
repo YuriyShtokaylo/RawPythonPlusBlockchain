9d363d36df6092f5d58db82783c9db1f44221f4e
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-10 12:31:31 +0100

    "access modifiers" =-(

--- Nice examples/oop/car.py
+++ Nice examples/oop/car.py
@@ -4,11 +4,18 @@ class Car:
 
     def __init__(self, top_speed=100):
         self.top_speed = top_speed
-        self.warnings = []
+        self.__warnings = []
 
     def __repr__(self):
         print('Printing...')
-        return 'Top Speed: {}, warnings: {}'.format(self.top_speed, len(self.warnings))
+        return 'Top Speed: {}, warnings: {}'.format(self.top_speed, len(self.__warnings))
+    
+    def add_warning(self, warning_text):
+        if len(warning_text) > 0:
+            self.__warnings.append(warning_text)
+    
+    def get_warnings(self):
+        return self.__warnings
     
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
@@ -17,21 +24,23 @@ class Car:
 car1 = Car()
 car1.drive()
 
-car1.warnings.append('New warning')
+car1.add_warning('New warning')
+hack = car1.get_warnings()
+hack.append([])#hack=-(
 print(car1)
 print(car1.__dict__)
 
 car2 = Car(200)
 car2.drive()
-print(car2.warnings)
+print(car2.get_warnings())
 
 car3 = Car()
 car3.warnings=['A']
 car3.drive()
-print(car3.warnings)
-print(car2.warnings)
-print(car1.warnings)
+print(car3.get_warnings())
+print(car2.get_warnings())
+print(car1.get_warnings())
 
 car4 = Car()
 car4.drive()
-print(car4.warnings)
+print(car4.get_warnings())
