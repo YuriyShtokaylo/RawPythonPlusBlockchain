b0b98f9498f74dc2456130a5d84aadfb6d89c1bf
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-10 11:35:50 +0100

    WAT?!

--- Nice examples/oop/car.py
+++ Nice examples/oop/car.py
@@ -1,20 +1,30 @@
 class Car:
-    top_speed = 100
-    warnings = []
+    # top_speed = 100
+    # warnings = []
+
+    def __init__(self, top_speed=100, warnings=[]):
+        self.top_speed = top_speed
+        self.warnings = warnings
 
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
+
 car1 = Car()
 car1.drive()
 
-Car.top_speed = 200
 car1.warnings.append('New warning')
 
-car2 = Car()
+car2 = Car(200)
 car2.drive()
 print(car2.warnings)
 
-car3 = Car()
+car3 = Car(warnings=['A'])
 car3.drive()
 print(car3.warnings)
+print(car2.warnings)
+print(car1.warnings)
+
+car4 = Car()
+car4.drive()
+print(car4.warnings)
