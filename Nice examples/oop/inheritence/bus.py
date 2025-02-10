d4a804e66f19b6be178f0edc033f68c359b18133
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-02-10 20:45:36 +0100

    Inheritance example

--- Nice examples/oop/inheritence/bus.py
+++ Nice examples/oop/inheritence/bus.py
@@ -1,29 +1,17 @@
-class Bus:
-    # top_speed = 100
-    # warnings = []
+from vehicle import Vehicle
 
+
+class Bus(Vehicle):
     def __init__(self, top_speed=100):
-        self.top_speed = top_speed
-        self.__warnings = []
+        super().__init__(top_speed)
         self.passangers = []
 
-    def __repr__(self):
-        print('Printing...')
-        return 'Top Speed: {}, warnings: {}'.format(self.top_speed, len(self.__warnings))
-    
-    def add_warning(self, warning_text):
-        if len(warning_text) > 0:
-            self.__warnings.append(warning_text)
-    
-    def get_warnings(self):
-        return self.__warnings
-    
-    def drive(self):
-        print('I am driving but certainly not faster than {}'.format(self.top_speed))
-        
     def add_group(self, passangers):
         self.passangers.extend(passangers)
-        
-bus = Bus(100)
+
+
+bus = Bus(150)
+bus.add_warning('1')
 bus.add_group([1, 2, 3])
 print(bus.passangers)
+bus.drive()
