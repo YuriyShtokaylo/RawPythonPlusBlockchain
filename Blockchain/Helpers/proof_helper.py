2db1d4aca812582d38a3a7f2f7f3456c231e1b7a
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-03-06 12:12:55 +0100

    Cleanup

--- Blockchain/Helpers/proof_helper.py
+++ Blockchain/Helpers/proof_helper.py
@@ -4,5 +4,4 @@ import hashlib as hl
 def valid_proof(transactions, last_hash, proof):
     guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
     guess_hash =hl.sha256(guess).hexdigest()
-    print(guess_hash)
     return guess_hash[0:2] == '00'
